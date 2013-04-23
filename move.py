2ccf343d4d5118d8c95a3a3f2c665b0f6e928532
jmartin <jmartin@zikzakmedia.com>
2013-04-23 12:41:54 +0200

    Added domain to account move line payment_type field. Adapted to pep8.

--- move.py
+++ move.py
@@ -1,7 +1,6 @@
-#This file is part of account_payment_type module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
+# This file is part of account_payment_type module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
@@ -9,16 +8,19 @@ from trytond.pyson import Eval
 __all__ = ['Line']
 __metaclass__ = PoolMeta
 
-_STATES = {
-    'readonly': Eval('state') != 'draft',
-}
-_DEPENDS = ['state']
 
 class Line:
     __name__ = 'account.move.line'
 
+    account_kind = fields.Function(fields.Selection([
+            ('payable', 'Payable'),
+            ('receivable', 'Receivable')
+            ], 'Kind'),
+        'get_account_kind')
     payment_type = fields.Many2One('account.payment.type',
-        'Payment Type', states=_STATES, depends=_DEPENDS)
+        'Payment Type', domain=[
+            ('kind', '=', Eval('account_kind')),
+            ], depends=['account_kind'])
 
     @classmethod
     def __setup__(cls):
@@ -40,3 +42,17 @@ class Line:
                 and self.account.kind not in ('payable', 'receivable')):
             self.raise_user_error('invalid_account_payment_type',
                 (self.rec_name,))
+
+    def get_account_kind(self, name):
+        if self.account.kind in ('payable', 'receivable'):
+            return self.account.kind
+        return 'none'
+
+    def on_change_account(self):
+        changes = super(Line, self).on_change_account()
+        if self.account and self.account.kind in ('payable', 'receivable'):
+            changes['account_kind'] = self.account.kind
+        else:
+            changes['account_kind'] = None
+        changes['payment_type'] = False
+        return changes
