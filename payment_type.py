d8dbc4a2beb4d5f2740a0cd90c22ec29e602a6b7
jmartin <jmartin@zikzakmedia.com>
2013-04-23 13:33:59 +0200

    Fixed error in domain of field company of model account.payment.type.

--- payment_type.py
+++ payment_type.py
@@ -3,6 +3,7 @@
 # the full copyright notices and license terms.
 
 from trytond.model import ModelView, ModelSQL, fields
+from trytond.pyson import Eval, If
 from trytond.transaction import Transaction
 
 __all__ = ['PaymentType']
@@ -17,10 +18,9 @@ class PaymentType(ModelSQL, ModelView):
     active = fields.Boolean('Active')
     company = fields.Many2One('company.company', 'Company', required=True,
         select=True, readonly=True, domain=[
-		    ('id', If(Eval('context', {}).contains('company'), '=', 
-				    '!='),
-			    Eval('context', {}).get('company', 0)),
-		    ])
+            ('id', If(Eval('context', {}).contains('company'), '=', '!='),
+                Eval('context', {}).get('company', 0)),
+            ])
     note = fields.Text('Description', translate=True,
         help=('Description of the payment type that will be shown in '
             'descriptions'))
