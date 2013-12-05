3303827a62a302ce5d099b4710aa7f743dc3b497
Guillem Barba <guillem@nan-tic.com>
2013-12-05 19:15:26 +0100

    Fix getter/setter of customer/supplier_payment_type

--- party.py
+++ party.py
@@ -13,12 +13,13 @@ class PartyAccountPaymentType(ModelSQL, ModelView):
     'Party Account Payment Type'
     __name__ = 'party.account.payment.type'
 
+    party = fields.Many2One('party.party', 'Party', required=True,
+        ondelete='CASCADE')
+    company = fields.Many2One('company.company', 'Company', ondelete='CASCADE')
     customer_payment_type = fields.Many2One('account.payment.type',
         'Customer Payment type')
     supplier_payment_type = fields.Many2One('account.payment.type',
         'Supplier Payment type')
-    company = fields.Many2One('company.company', 'Company')
-    party = fields.Many2One('party.party', 'Party')
 
 
 class Party:
@@ -34,50 +35,48 @@ class Party:
         'account.payment.type', string='Supplier Payment type', domain=[
                 ('kind', '=', 'payable'),
                 ],
-        help='Payment type of the supplier.'),
+            help='Payment type of the supplier.'),
         'get_payment_type', setter='set_payment_type')
 
     @classmethod
-    def get_payment_type(self, parties, names):
-        res = {}
+    def get_payment_type(cls, parties, names):
         PartyAccountPaymentType = Pool().get('party.account.payment.type')
         company = Transaction().context.get('company')
-        party = parties[0].id
-        party_account_payment_type = False
-        if company:
-            party_account_payment_type = PartyAccountPaymentType.search([
-                    ('company', '=', company),
-                    ('party', '=', party)
-                    ], limit=1)
-            if party_account_payment_type:
-                for field_name in set(names):
-                    value = getattr(party_account_payment_type[0], field_name)
-                    if value:
-                        res[field_name] = {party: value.id}
-                    else:
-                        res[field_name] = {party: False}
-        if not company or not party_account_payment_type:
-            for field_name in set(names):
-                res[field_name] = {party: False}
+
+        res = {}
+        party_ids = [p.id for p in parties]
+        for fname in names:
+            res[fname] = {}.fromkeys(party_ids, None)
+
+        party_account_payment_type = PartyAccountPaymentType.search([
+                ('company', '=', company),
+                ('party', 'in', parties)
+                ])
+        for payment_type_fields in party_account_payment_type:
+            party_id = payment_type_fields.party.id
+            for fname in set(names):
+                res[fname][party_id] = getattr(payment_type_fields, fname)
         return res
 
     @classmethod
     def set_payment_type(cls, parties, name, value):
         PartyAccountPaymentType = Pool().get('party.account.payment.type')
         company = Transaction().context.get('company')
-        party = parties[0]
-        if company:
-            party_account_payment_type = PartyAccountPaymentType.search([
-                    ('company', '=', company),
-                    ('party', '=', party.id)
-                    ], limit=1)
-            if not party_account_payment_type:
-                vlist = [{
-                        'company': company,
-                        'party': party.id,
-                        name: value,
-                        }]
-                PartyAccountPaymentType.create(vlist)
-            else:
-                ids = party_account_payment_type[0]
-                PartyAccountPaymentType.write([ids], {name: value})
+
+        party_payment_types = PartyAccountPaymentType.search([
+                ('company', '=', company),
+                ('party', 'in', parties)
+                ])
+        PartyAccountPaymentType.write(party_payment_types, {name: value})
+
+        vlist = []
+        parties_done_ids = [papt.party.id for papt in party_payment_types]
+        for missing_party_id in set(p.id
+                for p in parties if p.id not in parties_done_ids):
+            vlist.append({
+                    'party': missing_party_id,
+                    'company': company,
+                    name: value,
+                    })
+        if vlist:
+            PartyAccountPaymentType.create(vlist)
