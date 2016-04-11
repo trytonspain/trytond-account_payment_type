e23c178a02f6ff08c49db1ef19d8d3eb1c01a2f8
Guillem Barba <guillem@nan-tic.com>
2016-04-11 15:35:19 +0200

    Adapted tests to new way

--- tests/test_account_payment_type.py
+++ tests/test_account_payment_type.py
@@ -2,103 +2,84 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from decimal import Decimal
-import datetime
-import doctest
 import unittest
+
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+
+from trytond.modules.company.tests import create_company, set_company
+from trytond.modules.account.tests import create_chart, get_fiscalyear
+from trytond.modules.account_invoice.tests import set_invoice_sequences
 
 
 class AccountPaymentTypeTestCase(ModuleTestCase):
     'Test Account Payment Type module'
     module = 'account_payment_type'
 
-    def setUp(self):
-        super(AccountPaymentTypeTestCase, self).setUp()
-        self.account_template = POOL.get('account.account.template')
-        self.tax_code_template = POOL.get('account.tax.code.template')
-        self.tax_template = POOL.get('account.tax.code.template')
-        self.account = POOL.get('account.account')
-        self.account_create_chart = POOL.get(
-            'account.create_chart', type='wizard')
-        self.company = POOL.get('company.company')
-        self.user = POOL.get('res.user')
-        self.fiscalyear = POOL.get('account.fiscalyear')
-        self.sequence = POOL.get('ir.sequence')
-        self.sequence_strict = POOL.get('ir.sequence.strict')
-        self.move = POOL.get('account.move')
-        self.move_line = POOL.get('account.move.line')
-        self.payment_type = POOL.get('account.payment.type')
-        self.journal = POOL.get('account.journal')
-        self.account_type = POOL.get('account.account.type')
-
+    @with_transaction()
     def test0010move_lines(self):
         'Test account debit/credit'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin')])
-            fiscalyear, = self.fiscalyear.search([])
-            today = datetime.date.today()
-            invoice_sequence, = self.sequence_strict.create([{
-                        'name': '%s' % today.year,
-                        'code': 'account.invoice',
-                        'company': company.id,
-                        }])
-            fiscalyear.out_invoice_sequence = invoice_sequence
-            fiscalyear.in_invoice_sequence = invoice_sequence
-            fiscalyear.out_credit_note_sequence = invoice_sequence
-            fiscalyear.in_credit_note_sequence = invoice_sequence
+        pool = Pool()
+        Account = pool.get('account.account')
+        FiscalYear = pool.get('account.fiscalyear')
+        Journal = pool.get('account.journal')
+        Move = pool.get('account.move')
+        MoveLine = pool.get('account.move.line')
+        PaymentType = pool.get('account.payment.type')
+
+        company = create_company()
+        with set_company(company):
+            create_chart(company)
+            fiscalyear = get_fiscalyear(company)
+            set_invoice_sequences(fiscalyear)
             fiscalyear.save()
+            FiscalYear.create_period([fiscalyear])
             period = fiscalyear.periods[0]
-            journal_revenue, = self.journal.search([
+
+            journal_revenue, = Journal.search([
                     ('code', '=', 'REV'),
                     ])
-            revenue, = self.account.search([
+            revenue, = Account.search([
                     ('kind', '=', 'revenue'),
                     ])
-            receivable, = self.account.search([
+            receivable, = Account.search([
                     ('kind', '=', 'receivable'),
                     ])
-            payable, = self.account.search([
+            payable, = Account.search([
                     ('kind', '=', 'payable'),
                     ])
-            payment_payable, = self.payment_type.create([{
-                'name': 'Payment Payable',
-                'kind': 'payable',
-                'company': company.id,
-                }])
-            payment_receivable, = self.payment_type.create([{
-                'name': 'Payment Receivable',
-                'kind': 'receivable',
-                'company': company.id,
-                }])
-            move, = self.move.create([{
-                    'period': period.id,
-                    'journal': journal_revenue.id,
-                    'date': period.start_date,
-                    }])
-            self.move_line.create([{
-                    'move': move.id,
-                    'account': revenue.id,
-                    'debit': Decimal(30),
-                    }])
-            self.assertRaises(Exception, self.move_line.create, [{
-                    'move': move.id,
-                    'account': revenue.id,
-                    'debit': Decimal(30),
-                    'payment_type': payment_receivable,
-                    }])
+            payment_payable, = PaymentType.create([{
+                        'name': 'Payment Payable',
+                        'kind': 'payable',
+                        'company': company.id,
+                        }])
+            payment_receivable, = PaymentType.create([{
+                        'name': 'Payment Receivable',
+                        'kind': 'receivable',
+                        'company': company.id,
+                        }])
+            move, = Move.create([{
+                        'period': period.id,
+                        'journal': journal_revenue.id,
+                        'date': period.start_date,
+                        }])
+            MoveLine.create([{
+                        'move': move.id,
+                        'account': revenue.id,
+                        'debit': Decimal(30),
+                        }])
+            self.assertRaises(Exception, MoveLine.create, [{
+                        'move': move.id,
+                        'account': revenue.id,
+                        'debit': Decimal(30),
+                        'payment_type': payment_receivable,
+                        }])
             # TODO Create move line payment + payment type payable
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.account.tests import test_account
-    for test in test_account.suite():
-        if test not in suite and not isinstance(test, doctest.DocTestCase):
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentTypeTestCase))
     return suite
