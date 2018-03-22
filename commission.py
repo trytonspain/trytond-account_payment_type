4133925d9590a14565a4141f683964d2527c541d
?ngel ?lvarez <angel@nan-tic.com>
2018-03-22 11:48:28 +0100

    add __all__ on commision file

--- commission.py
+++ commission.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
-
+__all__ = ['Commission']
 
 class Commission:
     __metaclass__ = PoolMeta
